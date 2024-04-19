fbc188b6fbd0300c74d3aef39d32a9f52f0e5934
Alexander <alzahar@ukr.net>
2024-04-19 14:15:38 +0300

    stage 2 gallery

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 import makeFetch from "./js/pixabay-api"
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import makeGallery from "./js/render-functions";
 
 const form = document.querySelector(".search-form");
+const galleryContainer = document.querySelector(".gallery");
+
 form.addEventListener("submit", event => {
     event.preventDefault();
     const searchString = event.target.elements.search.value.trim();
     if (searchString === "") {
         return;
     }
+    galleryContainer.innerHTML = "";
     makeFetch(searchString)
         .then(data => {
             if (data.total == 0) {
                 iziToast.show(createEmptyMsg());
             }
+            galleryContainer.insertAdjacentHTML('beforeend', makeGallery(data.hits));
+
             console.log(data);
     })
     console.log(searchString);
